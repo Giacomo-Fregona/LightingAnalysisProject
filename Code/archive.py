a3ec27bccc4d9a64792862f5cfa5cccd6e69d913
Giacomo-Fregona <giacomofregona99@gmail.com>
2023-10-14 13:41:45 +0200

    Archive for storing circle objects in list saved to disk

---
+++ Code/archive.py
@@ -0,0 +1,37 @@
+"""
+File containing methods for saving and retrieving data coming from the lighting analysis
+"""
+
+import pickle
+from circle import circle
+
+
+class Archive(list):
+	REAL = './Archive/real.pkl'
+	PROMPT = './Archive/prompt.pkl'
+	VARIATION = './Archive/variation.pkl'
+
+	def __init__(self, file_name: str):
+		self.file_name = file_name
+		super().__init__()
+
+	@staticmethod
+	def load(which_archive):
+		with open(which_archive, 'rb') as file:
+			return pickle.load(file)
+
+	def save(self):
+		with open(self.file_name, 'wb') as file:
+			return pickle.dump(self, file)
+
+
+if __name__ == '__main__':
+	# Archive bootstrapping
+	real_archive = Archive(Archive.REAL)
+	real_archive.save()
+
+	prompt_archive = Archive(Archive.PROMPT)
+	prompt_archive.save()
+
+	variation_archive = Archive(Archive.VARIATION)
+	variation_archive.save()
