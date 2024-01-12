0024d8f727cf5b05e0f1ed26e3a7a6f5fbc7818e
DarioMaddaloni <dario.maddaloni@studenti.unitn.it>
2024-01-12 13:44:50 +0100

    Modifiche a figure_4.py

--- Code/figure_4.py
+++ Code/figure_4.py
@@ -6,157 +6,178 @@ from archive import Archive
 import sys
 sys.path.append('.')
 
-dataReal = Archive.load(Archive.REAL)
-dataPrompt = Archive.load(Archive.PROMPT)
+def rpv(): # r=real, p=prompt, v=variation
+	flag1 = ""
+	while True:
+		flag1 = input("real, prompt or variation? ").lower().strip()
+		if flag1 == "real" or flag1 == "prompt" or flag1 == "variation":
+			break
+		else:
+			print("Error: Answer must be real, prompt or variation")
+	flag2 = ""
+	
+	flag2_prompt = {"real": "prompt or variation", "prompt": "real or variation", "variation": "real or prompt"}[flag1]
+	while True:
+		flag2 = input(f"{flag2_prompt}? ").lower().strip()
+		if (flag2 == "real" or flag2 == "prompt" or flag2 == "variation") and (flag1 != flag2):
+			break
+		else:
+			print(f"Error: Answer must be {flag2_prompt}")
+	return [flag1, flag2]
 
-print("dataPrompt:")
-print(dataPrompt)
-print()
+[flag1, flag2] = rpv()
 
-print("dataReal:")
-print(dataReal)
-print()
+dataX = Archive.load({"real": Archive.REAL, "prompt": Archive.PROMPT, "variation": Archive.VARIATION}[flag1])
+dataY = Archive.load({"real": Archive.REAL, "prompt": Archive.PROMPT, "variation": Archive.VARIATION}[flag2])
 
-
-l00ListPrompt = [myC.l00 for myC in dataPrompt]
-l1m1ListPrompt = [[myC.l1m1[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l10ListPrompt = [[myC.l10[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l11ListPrompt = [[myC.l11[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l2m2ListPrompt = [[myC.l2m2[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l2m1ListPrompt = [[myC.l2m1[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l20ListPrompt = [[myC.l20[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l21ListPrompt = [[myC.l21[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-l22ListPrompt = [[myC.l22[k]/myC.l00[k] for k in range(3)] for myC in dataPrompt]
-
-l00ListReal = [myC.l00 for myC in dataReal]
-l1m1ListReal = [[myC.l1m1[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l10ListReal = [[myC.l10[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l11ListReal = [[myC.l11[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l2m2ListReal = [[myC.l2m2[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l2m1ListReal = [[myC.l2m1[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l20ListReal = [[myC.l20[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l21ListReal = [[myC.l21[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-l22ListReal = [[myC.l22[k]/myC.l00[k] for k in range(3)] for myC in dataReal]
-
-
-print(dataReal[0].image_id)
-
-#Real
-l1m1Q35Real = np.quantile(l1m1ListReal, 0.35, axis=0)
-l1m1Q50Real = np.quantile(l1m1ListReal, 0.5, axis=0)
-l1m1Q65Real = np.quantile(l1m1ListReal, 0.65, axis=0)
-print(f"l1m1Q35Real: {l1m1Q35Real}")
-print(f"l1m1Q50Real: {l1m1Q50Real}")
-print(f"l1m1Q65Real: {l1m1Q65Real}")
-print()
-l10Q35Real = np.quantile(l10ListReal, 0.35, axis=0)
-l10Q50Real = np.quantile(l10ListReal, 0.5, axis=0)
-l10Q65Real = np.quantile(l10ListReal, 0.65, axis=0)
-print(f"l10Q35Real: {l10Q35Real}")
-print(f"l10Q50Real: {l10Q50Real}")
-print(f"l10Q65Real: {l10Q65Real}")
-print()
-l11Q35Real = np.quantile(l11ListReal, 0.35, axis=0)
-l11Q50Real = np.quantile(l11ListReal, 0.5, axis=0)
-l11Q65Real = np.quantile(l11ListReal, 0.65, axis=0)
-print(f"l11Q35Real: {l11Q35Real}")
-print(f"l11Q50Real: {l11Q50Real}")
-print(f"l11Q65Real: {l11Q65Real}")
-print()
-l2m2Q35Real = np.quantile(l2m2ListReal, 0.35, axis=0)
-l2m2Q50Real = np.quantile(l2m2ListReal, 0.5, axis=0)
-l2m2Q65Real = np.quantile(l2m2ListReal, 0.65, axis=0)
-print(f"l2m2Q35Real: {l2m2Q35Real}")
-print(f"l2m2Q50Real: {l2m2Q50Real}")
-print(f"l2m2Q65Real: {l2m2Q65Real}")
-print()
-l2m1Q35Real = np.quantile(l2m1ListReal, 0.35, axis=0)
-l2m1Q50Real = np.quantile(l2m1ListReal, 0.5, axis=0)
-l2m1Q65Real = np.quantile(l2m1ListReal, 0.65, axis=0)
-print(f"l2m1Q35Real: {l2m1Q35Real}")
-print(f"l2m1Q50Real: {l2m1Q50Real}")
-print(f"l2m1Q65Real: {l2m1Q65Real}")
-print()
-l20Q35Real = np.quantile(l20ListReal, 0.35, axis=0)
-l20Q50Real = np.quantile(l20ListReal, 0.5, axis=0)
-l20Q65Real = np.quantile(l20ListReal, 0.65, axis=0)
-print(f"l20Q35Real: {l20Q35Real}")
-print(f"l20Q50Real: {l20Q50Real}")
-print(f"l20Q65Real: {l20Q65Real}")
-print()
-l21Q35Real = np.quantile(l21ListReal, 0.35, axis=0)
-l21Q50Real = np.quantile(l21ListReal, 0.5, axis=0)
-l21Q65Real = np.quantile(l21ListReal, 0.65, axis=0)
-print(f"l21Q35Real: {l21Q35Real}")
-print(f"l21Q50Real: {l21Q50Real}")
-print(f"l21Q65Real: {l21Q65Real}")
+print(f"{flag1}:")
+print(dataX) #print the Archive
 print()
 
-l22Q35Real = np.quantile(l22ListReal, 0.35, axis=0)
-l22Q50Real = np.quantile(l22ListReal, 0.5, axis=0)
-l22Q65Real = np.quantile(l22ListReal, 0.65, axis=0)
-print(f"l22Q35Real: {l22Q35Real}")
-print(f"l22Q50Real: {l22Q50Real}")
-print(f"l22Q65Real: {l22Q65Real}")
+print(f"{flag2}:")
+print(dataY) #print the Archive
 print()
 
-#Prompt
-l1m1Q35Prompt = np.quantile(l1m1ListPrompt, 0.35, axis=0)
-l1m1Q50Prompt = np.quantile(l1m1ListPrompt, 0.5, axis=0)
-l1m1Q65Prompt = np.quantile(l1m1ListPrompt, 0.65, axis=0)
-print(f"l1m1Q35Prompt: {l1m1Q35Prompt}")
-print(f"l1m1Q50Prompt: {l1m1Q50Prompt}")
-print(f"l1m1Q65Prompt: {l1m1Q65Prompt}")
+
+l00ListX = [myC.l00 for myC in dataX]
+l1m1ListX = [[myC.l1m1[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l10ListX = [[myC.l10[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l11ListX = [[myC.l11[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l2m2ListX = [[myC.l2m2[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l2m1ListX = [[myC.l2m1[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l20ListX = [[myC.l20[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l21ListX = [[myC.l21[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+l22ListX = [[myC.l22[k]/myC.l00[k] for k in range(3)] for myC in dataX]
+
+l00ListY = [myC.l00 for myC in dataY]
+l1m1ListY = [[myC.l1m1[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l10ListY = [[myC.l10[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l11ListY = [[myC.l11[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l2m2ListY = [[myC.l2m2[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l2m1ListY = [[myC.l2m1[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l20ListY = [[myC.l20[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l21ListY = [[myC.l21[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+l22ListY = [[myC.l22[k]/myC.l00[k] for k in range(3)] for myC in dataY]
+
+
+print(dataY[0].image_id)
+
+#Y
+l1m1Q35Y = np.quantile(l1m1ListY, 0.35, axis=0)
+l1m1Q50Y = np.quantile(l1m1ListY, 0.5, axis=0)
+l1m1Q65Y = np.quantile(l1m1ListY, 0.65, axis=0)
+print(f"l1m1Q35Y: {l1m1Q35Y}")
+print(f"l1m1Q50Y: {l1m1Q50Y}")
+print(f"l1m1Q65Y: {l1m1Q65Y}")
 print()
-l10Q35Prompt = np.quantile(l10ListPrompt, 0.35, axis=0)
-l10Q50Prompt = np.quantile(l10ListPrompt, 0.5, axis=0)
-l10Q65Prompt = np.quantile(l10ListPrompt, 0.65, axis=0)
-print(f"l10Q35Prompt: {l10Q35Prompt}")
-print(f"l10Q50Prompt: {l10Q50Prompt}")
-print(f"l10Q65Prompt: {l10Q65Prompt}")
+l10Q35Y = np.quantile(l10ListY, 0.35, axis=0)
+l10Q50Y = np.quantile(l10ListY, 0.5, axis=0)
+l10Q65Y = np.quantile(l10ListY, 0.65, axis=0)
+print(f"l10Q35Y: {l10Q35Y}")
+print(f"l10Q50Y: {l10Q50Y}")
+print(f"l10Q65Y: {l10Q65Y}")
 print()
-l11Q35Prompt = np.quantile(l11ListPrompt, 0.35, axis=0)
-l11Q50Prompt = np.quantile(l11ListPrompt, 0.5, axis=0)
-l11Q65Prompt = np.quantile(l11ListPrompt, 0.65, axis=0)
-print(f"l11Q35Prompt: {l11Q35Prompt}")
-print(f"l11Q50Prompt: {l11Q50Prompt}")
-print(f"l11Q65Prompt: {l11Q65Prompt}")
+l11Q35Y = np.quantile(l11ListY, 0.35, axis=0)
+l11Q50Y = np.quantile(l11ListY, 0.5, axis=0)
+l11Q65Y = np.quantile(l11ListY, 0.65, axis=0)
+print(f"l11Q35Y: {l11Q35Y}")
+print(f"l11Q50Y: {l11Q50Y}")
+print(f"l11Q65Y: {l11Q65Y}")
 print()
-l2m2Q35Prompt = np.quantile(l2m2ListPrompt, 0.35, axis=0)
-l2m2Q50Prompt = np.quantile(l2m2ListPrompt, 0.5, axis=0)
-l2m2Q65Prompt = np.quantile(l2m2ListPrompt, 0.65, axis=0)
-print(f"l2m2Q35Prompt: {l2m2Q35Prompt}")
-print(f"l2m2Q50Prompt: {l2m2Q50Prompt}")
-print(f"l2m2Q65Prompt: {l2m2Q65Prompt}")
+l2m2Q35Y = np.quantile(l2m2ListY, 0.35, axis=0)
+l2m2Q50Y = np.quantile(l2m2ListY, 0.5, axis=0)
+l2m2Q65Y = np.quantile(l2m2ListY, 0.65, axis=0)
+print(f"l2m2Q35Y: {l2m2Q35Y}")
+print(f"l2m2Q50Y: {l2m2Q50Y}")
+print(f"l2m2Q65Y: {l2m2Q65Y}")
 print()
-l2m1Q35Prompt = np.quantile(l2m1ListPrompt, 0.35, axis=0)
-l2m1Q50Prompt = np.quantile(l2m1ListPrompt, 0.5, axis=0)
-l2m1Q65Prompt = np.quantile(l2m1ListPrompt, 0.65, axis=0)
-print(f"l2m1Q35Prompt: {l2m1Q35Prompt}")
-print(f"l2m1Q50Prompt: {l2m1Q50Prompt}")
-print(f"l2m1Q65Prompt: {l2m1Q65Prompt}")
+l2m1Q35Y = np.quantile(l2m1ListY, 0.35, axis=0)
+l2m1Q50Y = np.quantile(l2m1ListY, 0.5, axis=0)
+l2m1Q65Y = np.quantile(l2m1ListY, 0.65, axis=0)
+print(f"l2m1Q35Y: {l2m1Q35Y}")
+print(f"l2m1Q50Y: {l2m1Q50Y}")
+print(f"l2m1Q65Y: {l2m1Q65Y}")
 print()
-l20Q35Prompt = np.quantile(l20ListPrompt, 0.35, axis=0)
-l20Q50Prompt = np.quantile(l20ListPrompt, 0.5, axis=0)
-l20Q65Prompt = np.quantile(l20ListPrompt, 0.65, axis=0)
-print(f"l20Q35Prompt: {l20Q35Prompt}")
-print(f"l20Q50Prompt: {l20Q50Prompt}")
-print(f"l20Q65Prompt: {l20Q65Prompt}")
+l20Q35Y = np.quantile(l20ListY, 0.35, axis=0)
+l20Q50Y = np.quantile(l20ListY, 0.5, axis=0)
+l20Q65Y = np.quantile(l20ListY, 0.65, axis=0)
+print(f"l20Q35Y: {l20Q35Y}")
+print(f"l20Q50Y: {l20Q50Y}")
+print(f"l20Q65Y: {l20Q65Y}")
 print()
-l21Q35Prompt = np.quantile(l21ListPrompt, 0.35, axis=0)
-l21Q50Prompt = np.quantile(l21ListPrompt, 0.5, axis=0)
-l21Q65Prompt = np.quantile(l21ListPrompt, 0.65, axis=0)
-print(f"l21Q35Prompt: {l21Q35Prompt}")
-print(f"l21Q50Prompt: {l21Q50Prompt}")
-print(f"l21Q65Prompt: {l21Q65Prompt}")
+l21Q35Y = np.quantile(l21ListY, 0.35, axis=0)
+l21Q50Y = np.quantile(l21ListY, 0.5, axis=0)
+l21Q65Y = np.quantile(l21ListY, 0.65, axis=0)
+print(f"l21Q35Y: {l21Q35Y}")
+print(f"l21Q50Y: {l21Q50Y}")
+print(f"l21Q65Y: {l21Q65Y}")
 print()
 
-l22Q35Prompt = np.quantile(l22ListPrompt, 0.35, axis=0)
-l22Q50Prompt = np.quantile(l22ListPrompt, 0.5, axis=0)
-l22Q65Prompt = np.quantile(l22ListPrompt, 0.65, axis=0)
-print(f"l22Q35Prompt: {l22Q35Prompt}")
-print(f"l22Q50Prompt: {l22Q50Prompt}")
-print(f"l22Q65Prompt: {l22Q65Prompt}")
+l22Q35Y = np.quantile(l22ListY, 0.35, axis=0)
+l22Q50Y = np.quantile(l22ListY, 0.5, axis=0)
+l22Q65Y = np.quantile(l22ListY, 0.65, axis=0)
+print(f"l22Q35Y: {l22Q35Y}")
+print(f"l22Q50Y: {l22Q50Y}")
+print(f"l22Q65Y: {l22Q65Y}")
+print()
+
+#X
+l1m1Q35X = np.quantile(l1m1ListX, 0.35, axis=0)
+l1m1Q50X = np.quantile(l1m1ListX, 0.5, axis=0)
+l1m1Q65X = np.quantile(l1m1ListX, 0.65, axis=0)
+print(f"l1m1Q35X: {l1m1Q35X}")
+print(f"l1m1Q50X: {l1m1Q50X}")
+print(f"l1m1Q65X: {l1m1Q65X}")
+print()
+l10Q35X = np.quantile(l10ListX, 0.35, axis=0)
+l10Q50X = np.quantile(l10ListX, 0.5, axis=0)
+l10Q65X = np.quantile(l10ListX, 0.65, axis=0)
+print(f"l10Q35X: {l10Q35X}")
+print(f"l10Q50X: {l10Q50X}")
+print(f"l10Q65X: {l10Q65X}")
+print()
+l11Q35X = np.quantile(l11ListX, 0.35, axis=0)
+l11Q50X = np.quantile(l11ListX, 0.5, axis=0)
+l11Q65X = np.quantile(l11ListX, 0.65, axis=0)
+print(f"l11Q35X: {l11Q35X}")
+print(f"l11Q50X: {l11Q50X}")
+print(f"l11Q65X: {l11Q65X}")
+print()
+l2m2Q35X = np.quantile(l2m2ListX, 0.35, axis=0)
+l2m2Q50X = np.quantile(l2m2ListX, 0.5, axis=0)
+l2m2Q65X = np.quantile(l2m2ListX, 0.65, axis=0)
+print(f"l2m2Q35X: {l2m2Q35X}")
+print(f"l2m2Q50X: {l2m2Q50X}")
+print(f"l2m2Q65X: {l2m2Q65X}")
+print()
+l2m1Q35X = np.quantile(l2m1ListX, 0.35, axis=0)
+l2m1Q50X = np.quantile(l2m1ListX, 0.5, axis=0)
+l2m1Q65X = np.quantile(l2m1ListX, 0.65, axis=0)
+print(f"l2m1Q35X: {l2m1Q35X}")
+print(f"l2m1Q50X: {l2m1Q50X}")
+print(f"l2m1Q65X: {l2m1Q65X}")
+print()
+l20Q35X = np.quantile(l20ListX, 0.35, axis=0)
+l20Q50X = np.quantile(l20ListX, 0.5, axis=0)
+l20Q65X = np.quantile(l20ListX, 0.65, axis=0)
+print(f"l20Q35X: {l20Q35X}")
+print(f"l20Q50X: {l20Q50X}")
+print(f"l20Q65X: {l20Q65X}")
+print()
+l21Q35X = np.quantile(l21ListX, 0.35, axis=0)
+l21Q50X = np.quantile(l21ListX, 0.5, axis=0)
+l21Q65X = np.quantile(l21ListX, 0.65, axis=0)
+print(f"l21Q35X: {l21Q35X}")
+print(f"l21Q50X: {l21Q50X}")
+print(f"l21Q65X: {l21Q65X}")
+print()
+
+l22Q35X = np.quantile(l22ListX, 0.35, axis=0)
+l22Q50X = np.quantile(l22ListX, 0.5, axis=0)
+l22Q65X = np.quantile(l22ListX, 0.65, axis=0)
+print(f"l22Q35X: {l22Q35X}")
+print(f"l22Q50X: {l22Q50X}")
+print(f"l22Q65X: {l22Q65X}")
 print()
 
 
@@ -165,58 +186,57 @@ print()
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
 for i in range(3):
-
-	#Real and Prompt
-	axes[i].plot(l1m1Q50Real[i], l1m1Q50Prompt[i], c=[0, 0, 0, 1], marker="o")
-	axes[i].plot(l10Q50Real[i], l10Q50Prompt[i], c=[1, 0, 0, 1], marker="o")
-	axes[i].plot(l11Q50Real[i], l11Q50Prompt[i], c=[0, 1, 0, 1], marker="o")
-	axes[i].plot(l2m2Q50Real[i], l2m2Q50Prompt[i], c=[1, 1, 0, 1], marker="o")
-	axes[i].plot(l2m1Q50Real[i], l2m1Q50Prompt[i], c=[0, 0, 1, 1], marker="o")
-	axes[i].plot(l20Q50Real[i], l20Q50Prompt[i], c=[1, 0, 1, 1], marker="o")
-	axes[i].plot(l21Q50Real[i], l21Q50Prompt[i], c=[0, 1, 1, 1], marker="o")
-	axes[i].plot(l22Q50Real[i], l22Q50Prompt[i], c=[0, 0, 0, 0.5], marker="o")
+	
+	#Y and X
+	axes[i].plot(l1m1Q50Y[i], l1m1Q50X[i], c=[0, 0, 1, 1], marker="o", label="l(1,-1)")
+	axes[i].plot(l10Q50Y[i], l10Q50X[i], c=[1, 0, 0, 1], marker="o", label="l(1,0)")
+	axes[i].plot(l11Q50Y[i], l11Q50X[i], c=[1, 0.9, 0.1, 1], marker="o", label="l(1,1)")
+	axes[i].plot(l2m2Q50Y[i], l2m2Q50X[i], c=[1, 0, 1, 1], marker="o", label="l(2,-2)")
+	axes[i].plot(l2m1Q50Y[i], l2m1Q50X[i], c=[0, 1, 0, 1], marker="o", label="l(2,-1)")
+	axes[i].plot(l20Q50Y[i], l20Q50X[i], c=[0, 1, 1, 1], marker="o", label="l(2,0)")
+	axes[i].plot(l21Q50Y[i], l21Q50X[i], c=[0.7, 0, 0, 1], marker="o", label="l(2,1)")
+	axes[i].plot(l22Q50Y[i], l22Q50X[i], c=[0.5, 0.5, 0.5, 1], marker="o", label="l(2,2)")
 
 
 	lines = []
 	c = []
-	# Real and Prompt
-	lines.append([(l1m1Q35Real[i], l1m1Q50Prompt[i]), (l1m1Q65Real[i], l1m1Q50Prompt[i])])
-	lines.append([(l1m1Q50Real[i], l1m1Q35Prompt[i]), (l1m1Q50Real[i], l1m1Q65Prompt[i])])
-	c.append([0, 0, 0, 1])
-	c.append([0, 0, 0, 1])
-	lines.append([(l10Q35Real[i], l10Q50Prompt[i]), (l10Q65Real[i], l10Q50Prompt[i])])
-	lines.append([(l10Q50Real[i], l10Q35Prompt[i]), (l10Q50Real[i], l10Q65Prompt[i])])
-	c.append([1, 0, 0, 1])
-	c.append([1, 0, 0, 1])
-	lines.append([(l11Q35Real[i], l11Q50Prompt[i]), (l11Q65Real[i], l11Q50Prompt[i])])
-	lines.append([(l11Q50Real[i], l11Q35Prompt[i]), (l11Q50Real[i], l11Q65Prompt[i])])
-	c.append([0, 1, 0, 1])
-	c.append([0, 1, 0, 1])
-	lines.append([(l2m2Q35Real[i], l2m2Q50Prompt[i]), (l2m2Q65Real[i], l2m2Q50Prompt[i])])
-	lines.append([(l2m2Q50Real[i], l2m2Q35Prompt[i]), (l2m2Q50Real[i], l2m2Q65Prompt[i])])
-	c.append([1, 1, 0, 1])
-	c.append([1, 1, 0, 1])
-	lines.append([(l2m1Q35Real[i], l2m1Q50Prompt[i]), (l2m1Q65Real[i], l2m1Q50Prompt[i])])
-	lines.append([(l2m1Q50Real[i], l2m1Q35Prompt[i]), (l2m1Q50Real[i], l2m1Q65Prompt[i])])
+	# Y and X
+	lines.append([(l1m1Q35Y[i], l1m1Q50X[i]), (l1m1Q65Y[i], l1m1Q50X[i])])
+	lines.append([(l1m1Q50Y[i], l1m1Q35X[i]), (l1m1Q50Y[i], l1m1Q65X[i])])
 	c.append([0, 0, 1, 1])
 	c.append([0, 0, 1, 1])
-	lines.append([(l20Q35Real[i], l20Q50Prompt[i]), (l20Q65Real[i], l20Q50Prompt[i])])
-	lines.append([(l20Q50Real[i], l20Q35Prompt[i]), (l20Q50Real[i], l20Q65Prompt[i])])
+	lines.append([(l10Q35Y[i], l10Q50X[i]), (l10Q65Y[i], l10Q50X[i])])
+	lines.append([(l10Q50Y[i], l10Q35X[i]), (l10Q50Y[i], l10Q65X[i])])
+	c.append([1, 0, 0, 1])
+	c.append([1, 0, 0, 1])
+	lines.append([(l11Q35Y[i], l11Q50X[i]), (l11Q65Y[i], l11Q50X[i])])
+	lines.append([(l11Q50Y[i], l11Q35X[i]), (l11Q50Y[i], l11Q65X[i])])
+	c.append([1, 0.9, 0.1, 1])
+	c.append([1, 0.9, 0.1, 1])
+	lines.append([(l2m2Q35Y[i], l2m2Q50X[i]), (l2m2Q65Y[i], l2m2Q50X[i])])
+	lines.append([(l2m2Q50Y[i], l2m2Q35X[i]), (l2m2Q50Y[i], l2m2Q65X[i])])
 	c.append([1, 0, 1, 1])
 	c.append([1, 0, 1, 1])
-	lines.append([(l21Q35Real[i], l21Q50Prompt[i]), (l21Q65Real[i], l21Q50Prompt[i])])
-	lines.append([(l21Q50Real[i], l21Q35Prompt[i]), (l21Q50Real[i], l21Q65Prompt[i])])
+	lines.append([(l2m1Q35Y[i], l2m1Q50X[i]), (l2m1Q65Y[i], l2m1Q50X[i])])
+	lines.append([(l2m1Q50Y[i], l2m1Q35X[i]), (l2m1Q50Y[i], l2m1Q65X[i])])
+	c.append([0, 1, 0, 1])
+	c.append([0, 1, 0, 1])
+	lines.append([(l20Q35Y[i], l20Q50X[i]), (l20Q65Y[i], l20Q50X[i])])
+	lines.append([(l20Q50Y[i], l20Q35X[i]), (l20Q50Y[i], l20Q65X[i])])
 	c.append([0, 1, 1, 1])
 	c.append([0, 1, 1, 1])
-	lines.append([(l22Q35Real[i], l22Q50Prompt[i]), (l22Q65Real[i], l22Q50Prompt[i])])
-	lines.append([(l22Q50Real[i], l22Q35Prompt[i]), (l22Q50Real[i], l22Q65Prompt[i])])
-	c.append([0, 0, 0, 0.5])
-	c.append([0, 0, 0, 0.5])
+	lines.append([(l21Q35Y[i], l21Q50X[i]), (l21Q65Y[i], l21Q50X[i])])
+	lines.append([(l21Q50Y[i], l21Q35X[i]), (l21Q50Y[i], l21Q65X[i])])
+	c.append([0.7, 0, 0, 1])
+	c.append([0.7, 0, 0, 1])
+	lines.append([(l22Q35Y[i], l22Q50X[i]), (l22Q65Y[i], l22Q50X[i])])
+	lines.append([(l22Q50Y[i], l22Q35X[i]), (l22Q50Y[i], l22Q65X[i])])
+	c.append([0.5, 0.5, 0.5, 1])
+	c.append([0.5, 0.5, 0.5, 1])
 
 
 
-	lc = mc.LineCollection(lines, colors=c, linewidths=2)
-
+	lc = mc.LineCollection(lines, colors=c, linewidths=1.5)
 
 	axes[i].add_collection(lc)
 	axes[i].autoscale()
@@ -230,5 +250,13 @@ for i in range(3):
 	axes[i].set_aspect('equal')
 	axes[i].set_xlim(lims)
 	axes[i].set_ylim(lims)
-
+	axes[i].legend(loc="upper left")
+	
+	if i==0:
+		axes[i].set_title(label="Red layer")
+	elif i==1:
+		axes[i].set_title("Green layer")
+	elif i==2:
+		axes[i].set_title("Blue layer")
+	
 plt.show()
