4f3a44a313cb92ba89d671fe676cdb0f54732bd2
Giacomo-Fregona <giacomofregona99@gmail.com>
2024-01-30 18:59:11 +0100

    Meeting updates

--- Code/stima_direzione.py
+++ Code/stima_direzione.py
@@ -1,6 +1,9 @@
+from PIL import Image
+
 import circle
 import pickle
 import numpy as np
+from interaction_lib import interactiveGuess
 # Borderline cases are not managed (e.g. case with np.arctan(1/0)). Indeed, they are extremely rare and would make the code much more difficult to read
 
 def positionSphereFromCamera(C, FOVX=90, FOVY=90, pixelLength = 1024): #FOV=field of view
@@ -21,60 +24,88 @@ def estimationPositionLightFromSphereFromCamera(SphereFromCamera, LightfromSpher
 	return [270 + SphereFromCamera[0] - LightfromSphere[0], 180 + SphereFromCamera[1] - LightfromSphere[1]]
 
 
+# if __name__ == '__main__':
+	# flag = ""
+	# while True:
+	# 	flag = input("real, prompt or variation? ").lower().strip()
+	# 	if flag == "real" or flag == "prompt" or flag == "variation":
+	# 		break
+	# 	else:
+	# 		print("Error: Answer must be real or dalle2")
+	#
+	# fileToOpen = "./Archive/"+flag+".pkl"
+	# with open(fileToOpen, "rb") as file:
+	# 	data = pickle.load(file)
+	# file.close()
+	
+	
+	# listOfCircleInSameImage = [data[0]]
+	# for i in range(1, len(data)):
+	# 	if data[i].image_id == listOfCircleInSameImage[0].image_id:
+	# 		listOfCircleInSameImage.append(data[i])
+	# 	else:
+	# 		if len(listOfCircleInSameImage) == 1:
+	# 			print(listOfCircleInSameImage[0].image_id)
+	# 			print("NON CONTARE: UNA SOLA SFERA NELL'IMMAGINE")
+	# 			print()
+	# 		else:
+	# 			print(f"Image: {listOfCircleInSameImage[0].image_id}")
+	# 			GeneralR = []
+	# 			GeneralG = []
+	# 			GeneralB = []
+	# 			for j in range(len(listOfCircleInSameImage)):
+	# 				print(f"Circle: {listOfCircleInSameImage[j].center}, {listOfCircleInSameImage[j].r}")
+	# 				sfc = positionSphereFromCamera(listOfCircleInSameImage[j])
+	# 				print(f"Sphere from Camera: {sfc}")
+	# 				lfs = positionLightfromSphereRGB(listOfCircleInSameImage[j])
+	# 				print(f"Light from SphereR: {lfs[0]}")
+	# 				print(f"Light from SphereG: {lfs[1]}")
+	# 				print(f"Light from SphereB: {lfs[2]}")
+	# 				lfcR = estimationPositionLightFromSphereFromCamera(sfc, lfs[0])
+	# 				GeneralR.append(lfcR)
+	# 				lfcG = estimationPositionLightFromSphereFromCamera(sfc, lfs[1])
+	# 				GeneralG.append(lfcG)
+	# 				lfcB = estimationPositionLightFromSphereFromCamera(sfc, lfs[2])
+	# 				GeneralB.append(lfcB)
+	# 				print(f"Light from Camera R: {lfcR}")
+	# 				print(f"Light from Camera G: {lfcG}")
+	# 				print(f"Light from Camera B: {lfcB}")
+	# 				print()
+	# 			print(f"Rvar : {np.std(GeneralR, axis=0)}")
+	# 			print(f"Gvar : {np.std(GeneralG, axis=0)}")
+	# 			print(f"Bvar : {np.std(GeneralB, axis=0)}")
+	# 			print()
+	#
+	# 		listOfCircleInSameImage = [data[i]]
 if __name__ == '__main__':
-	flag = ""
-	while True:
-		flag = input("real, prompt or variation? ").lower().strip()
-		if flag == "real" or flag == "prompt" or flag == "variation":
-			break
-		else:
-			print("Error: Answer must be real or dalle2")
+	originalImage = np.asarray(Image.open('./Samples/real/test.jpg'), dtype=np.uint8)
 
-	fileToOpen = "./Archive/"+flag+".pkl"
-	with open(fileToOpen, "rb") as file:
-		data = pickle.load(file)
-	file.close()
-	
-	
-	listOfCircleInSameImage = [data[0]]
-	for i in range(1, len(data)):
-		if data[i].image_id == listOfCircleInSameImage[0].image_id:
-			listOfCircleInSameImage.append(data[i])
-		else:
-			if len(listOfCircleInSameImage) == 1:
-				print(listOfCircleInSameImage[0].image_id)
-				print("NON CONTARE: UNA SOLA SFERA NELL'IMMAGINE")
-				print()
-			else:
-				print(f"Image: {listOfCircleInSameImage[0].image_id}")
-				GeneralR = []
-				GeneralG = []
-				GeneralB = []
-				for j in range(len(listOfCircleInSameImage)):
-					print(f"Circle: {listOfCircleInSameImage[j].center}, {listOfCircleInSameImage[j].r}")
-					sfc = positionSphereFromCamera(listOfCircleInSameImage[j])
-					print(f"Sphere from Camera: {sfc}")
-					lfs = positionLightfromSphereRGB(listOfCircleInSameImage[j])
-					print(f"Light from SphereR: {lfs[0]}")
-					print(f"Light from SphereG: {lfs[1]}")
-					print(f"Light from SphereB: {lfs[2]}")
-					lfcR = estimationPositionLightFromSphereFromCamera(sfc, lfs[0])
-					GeneralR.append(lfcR)
-					lfcG = estimationPositionLightFromSphereFromCamera(sfc, lfs[1])
-					GeneralG.append(lfcG)
-					lfcB = estimationPositionLightFromSphereFromCamera(sfc, lfs[2])
-					GeneralB.append(lfcB)
-					print(f"Light from Camera R: {lfcR}")
-					print(f"Light from Camera G: {lfcG}")
-					print(f"Light from Camera B: {lfcB}")
-					print()
-				print(f"Rvar : {np.std(GeneralR, axis=0)}")
-				print(f"Gvar : {np.std(GeneralG, axis=0)}")
-				print(f"Bvar : {np.std(GeneralB, axis=0)}")
-				print()
-			
-			listOfCircleInSameImage = [data[i]]
 
+	C1: circle
+	C1 = interactiveGuess(originalImage)
+	C1.estimateCoefficients(image=originalImage, M=-1)
+	C2: circle
+	C2 = interactiveGuess(originalImage)
+	C2.estimateCoefficients(image=originalImage, M=-1)
+
+	sfc1 = positionSphereFromCamera(C1, pixelLength=1920)
+	sfc2 = positionSphereFromCamera(C2, pixelLength=1920)
+
+	lfs1 = positionLightfromSphereRGB(C1)
+	lfs2 = positionLightfromSphereRGB(C2)
+
+	lfcR1 = estimationPositionLightFromSphereFromCamera(sfc1, lfs1[0])
+	lfcR2 = estimationPositionLightFromSphereFromCamera(sfc2, lfs1[0])
+
+	lfcG1 = estimationPositionLightFromSphereFromCamera(sfc1, lfs1[1])
+	lfcG2 = estimationPositionLightFromSphereFromCamera(sfc2, lfs2[1])
+
+	lfcB1 = estimationPositionLightFromSphereFromCamera(sfc1, lfs1[2])
+	lfcB2 = estimationPositionLightFromSphereFromCamera(sfc2, lfs2[2])
+
+	print(f'{lfcR1} |||| {lfcR2} ---- {lfs1[0]} |||| {lfs2[0]}')
+	print(f'{lfcG1} |||| {lfcG2} ---- {lfs1[1]} |||| {lfs2[1]}')
+	print(f'{lfcB1} |||| {lfcB2} ---- {lfs1[2]} |||| {lfs2[2]}')
 
 
 
